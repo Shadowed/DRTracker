ed19863869fa7d1b61eb62186b6cd2d13655122c
Shadowed <shadowed.wow@gmail.com>
2009-07-29 01:26:00 -0700

    * Definitely need GTB still

--- libs/update.bat
+++ libs/update.bat
@@ -7,7 +7,7 @@ svn export svn://svn.wowace.com/wow/ace3/mainline/trunk/CallbackHandler-1.0 --fo
 svn export svn://svn.wowace.com/wow/ace-gui-3-0-shared-media-widgets/mainline/trunk/AceGUI-3.0-SharedMediaWidgets --force
 svn export svn://svn.wowace.com/wow/libsharedmedia-3-0/mainline/trunk/LibSharedMedia-3.0 --force LibSharedMedia-3.0
 rmdir /Q /S GTB-1.0
-git clone git://git.wowace.com/wow/gtb-10/mainline.git GTB-1.0
+git clone git://git.wowace.com/wow/gtb-1-0/mainline.git GTB-1.0
 rmdir /Q /S DRData-1.0
 git clone git://git.wowace.com/wow/drdata-10/mainline.git DRData-1.0
 @PAUSE
